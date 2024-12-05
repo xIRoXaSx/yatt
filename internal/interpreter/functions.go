1546df080644da12e2dc1e5a2f8617724e89b4c0
xIRoXaSx <38859398+xIRoXaSx@users.noreply.github.com>
2024-12-06 00:03:52 +0100

    Added env var function.

--- internal/interpreter/functions.go
+++ internal/interpreter/functions.go
@@ -3,6 +3,7 @@ package interpreter
 import (
 	"bytes"
 	"fmt"
+	"os"
 	"path/filepath"
 	"strings"
 
@@ -13,6 +14,7 @@ import (
 )
 
 const (
+	functionEnvVar       = "env"
 	functionFileBaseName = "fbasename"
 	functionFileName     = "fname"
 	functionLower        = "lower"
@@ -59,6 +61,9 @@ func (i *Interpreter) executeFunction(function string, args [][]byte, fileName s
 	switch function {
 	case functionFileName:
 		ret = []byte(fileName)
+
+	case functionEnvVar:
+		ret = []byte(os.Getenv(string(args[0])))
 
 	case functionFileBaseName:
 		ret = []byte(filepath.Base(fileName))
