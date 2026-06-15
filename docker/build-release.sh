d22086711d37485b7f75560dc05ed937296a6999
xIRoXaSx <38859398+xIRoXaSx@users.noreply.github.com>
2026-06-15 19:12:28 +0200

    chore(format): indentation.

--- docker/build-release.sh
+++ docker/build-release.sh
@@ -8,20 +8,20 @@ arch=("386" "amd64" "arm" "arm64")
 binName="${PROJECT:-yatt}"
 
 for o in "${os[@]}" ; do
-  for a in "${arch[@]}" ; do
-    # Skip certain os - architecture pairs.
-    [[ "${o}" == "freebsd" || "${o}" == "openbsd" && "${a}" == "arm64" ]] && continue
-    [[ "${o}" == "windows" && "${a}" == "arm"* ]] && continue
+    for a in "${arch[@]}" ; do
+        # Skip certain os - architecture pairs.
+        [[ "${o}" == "freebsd" || "${o}" == "openbsd" && "${a}" == "arm64" ]] && continue
+        [[ "${o}" == "windows" && "${a}" == "arm"* ]] && continue
 
-    # Add extension for windows.
-    ext=""
-    [[ "${o}" == "windows" ]] && ext=".exe"
+        # Add extension for windows.
+        ext=""
+        [[ "${o}" == "windows" ]] && ext=".exe"
 
-    echo "Building ${o}/${a}..."
-    out="${binName}${ext}"
-    CGO_ENABLED=0 GOOS=${o} GOARCH=${a} go build -buildvcs=false -ldflags "-w -extldflags '-static'" -o "/work/${out}"
-    echo "Zipping..."
-    zip -j "/build/${binName}-${VERSION}_${o}_${a}.zip" "/work/${out}"
-    echo "---"
-  done
+        echo "Building ${o}/${a}..."
+        out="${binName}${ext}"
+        CGO_ENABLED=0 GOOS=${o} GOARCH=${a} go build -buildvcs=false -ldflags "-w -extldflags '-static'" -o "/work/${out}"
+        echo "Zipping..."
+        zip -j "/build/${binName}-${VERSION}_${o}_${a}.zip" "/work/${out}"
+        echo "---"
+    done
 done
